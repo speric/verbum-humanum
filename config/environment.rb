04f2d9d8180f48064ac797846362e5100aa4c037
speric <eric@prudentiadigital.com>
2010-05-05 09:39:28 -0400

    Amazon ECS stuff

--- config/environment.rb
+++ config/environment.rb
@@ -7,8 +7,18 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem "hpricot"
-  config.gem "amazon-ecs"
+  #config.gem "hpricot"
+  #config.gem "amazon-ecs"
   
   config.time_zone = 'Eastern Time (US & Canada)'
+end
+
+require 'amazon/ecs'
+require 'yaml'
+
+amazon_api_keys = YAML.load_file("#{RAILS_ROOT}/config/amazon_api_keys.yml")
+
+Amazon::Ecs.configure do |options|
+  options[:aWS_access_key_id] = amazon_api_keys["aws_access_key_id"]
+  options[:aWS_secret_key] = amazon_api_keys["aws_secret_key"]
 end
