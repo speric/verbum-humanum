590b4fdb8d6921bb6b773613bbb7f20f60f2dd10
speric <eric@prudentiadigital.com>
2010-05-05 10:32:08 -0400

    Detail listing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,4 +9,8 @@ class HomeController < ApplicationController
       @items = Amazon::Ecs.item_search("C.S. Lewis", {:response_group => 'Medium', :sort => 'salesrank'})
     end
   end
+  
+  def book
+    @item = Amazon::Ecs.item_lookup(params[:id], {:response_group => 'ItemAttributes'})
+  end
 end
