4e232386b1f408fda7ea51d3c220e26ab282ac0f
speric <eric@prudentiadigital.com>
2010-05-05 10:38:42 -0400

    Fixed search bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,10 @@
 class HomeController < ApplicationController
   def index
-    puts amazon_api_keys["aws_access_key_id"]
-    puts amazon_api_keys["aws_secret_key"]
   end
   
   def search
     if request.post?
-      @items = Amazon::Ecs.item_search("C.S. Lewis", {:response_group => 'Medium', :sort => 'salesrank'})
+      @items = Amazon::Ecs.item_search(params[:search_term], {:response_group => 'Medium', :sort => 'salesrank'})
     end
   end
   
